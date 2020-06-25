8de3fa1a41d900a5b2bc3b0e552cb96c34e2203f
Josh Rabinowitz <joshr@joshr.com>
2020-06-24 22:26:35 -0400

    Mojo::mysql, not Mojo::MySQL

--- yancytest.pl
+++ yancytest.pl
@@ -6,7 +6,8 @@ use Getopt::Long;
 use File::Basename;
 use File::Slurper qw(read_lines);
 use Mojolicious::Lite;
-use Mojo::MySQL; # Supported backends: Pg, MySQL, SQLite, DBIx::Class
+use Mojo::mysql; # Supported backends: Pg, Mysql, SQLite, DBIx::Class
+                # note: don't use ancient "Mojo::MySQL"
 
 my ($mojo_mysql_connectstring) = read_lines( "/home/$ENV{USER}/.mojo-mysql-connectstring.txt" );
 chomp($mojo_mysql_connectstring);
@@ -15,7 +16,7 @@ chomp($mojo_mysql_connectstring);
 plugin Yancy => {
     # our MySQL connector
     # example connect string looks like: 'mysql://username:password@hostname/test;mysql_ssl=1'
-    backend => { mysql => Mojo::MySQL->new( $mojo_mysql_connectstring ) },
+    backend => { mysql => Mojo::mysql->new( $mojo_mysql_connectstring ) },
     read_schema => 1,
 };
 
